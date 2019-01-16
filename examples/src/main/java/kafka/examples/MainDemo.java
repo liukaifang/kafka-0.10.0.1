25cfcc4624dd9f36a59389977fcd6ce11169e62a
刘开放 <liu.kaifang@sf-express.com>
2019-01-16 18:05:24 +0800

    注释

--- examples/src/main/java/kafka/examples/MainDemo.java
+++ examples/src/main/java/kafka/examples/MainDemo.java
@@ -1,14 +1,16 @@
 package kafka.examples;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
+import org.junit.Test;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class MainDemo {
 
-    public static void main(String[] args) {
-
-//        AtomicInteger a = new AtomicInteger(12);
+    @Test
+    public void test0() {
+ //        AtomicInteger a = new AtomicInteger(12);
 //        System.out.println(a.compareAndSet(14, 13));
 //        System.out.println(a);
         final AtomicLong currentThread = new AtomicLong(2L);
@@ -17,4 +19,27 @@ public class MainDemo {
             System.out.println("====================" + currentThread.get());
         }
     }
+
+    @Test
+    public void testQueue() {
+        ArrayBlockingQueue queue = new ArrayBlockingQueue<String>(2);
+        queue.add("a");
+        queue.add("b");
+        queue.add("c");
+        try {
+            queue.take();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(queue.toArray());
+    }
+
+    @Test
+    public void test() {
+        ConcurrentHashMap<Object, Object> map = new ConcurrentHashMap<>();
+        map.put("a", 123);
+        System.out.println(map.putIfAbsent("a", 22));
+        System.out.println(map.putIfAbsent("b", 22));
+    }
+
 }
